5c17aad44e0e190be25f16295926d6fa92b6dd2d
João Sena <joasoenadainese@gmail.com>
2022-04-21 19:25:25 -0300

    implementação login e salvando token no async storage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import { TextInput, TouchableOpacity,Text } from 'react-native';
 import { View, KeyboardAvoidingView,Image } from 'react-native';
 import HeaderComponent from '../components/HeaderComponent';
 import BodyComponent from '../components/BodyComponent';
 import StylesLoginPage from '../styles/StylesLoginPage';
+import { login } from '../services/auth-service';
+import { useUser } from '../contexts/UserContext';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const LoginPage = () => {
+  const {setUserSigned, setUserName, setUserId} = useUser();
+
+  const [email, setEmail] = useState('joao@gmail.com');
+  const [password, setPassword] = useState('abc123');
+
+  const handleLogin = () => {
+    login({
+      email: email,
+      password: password
+    }).then( response => {
+      if(response && response.success){
+        console.log("Login success")
+        setUserSigned(true);
+        setUserName(response.data.user.name);
+        setUserId(response.data.user.id);
+        AsyncStorage.setItem('@TOKEN_KEY', response.accessToken).then();
+      }else{
+        console.log("Login failed");
+      }
+    })
+  }
+
   return (
     <>
     <HeaderComponent></HeaderComponent>
@@ -19,17 +45,17 @@ const LoginPage = () => {
             <TextInput style={StylesLoginPage.Interface}
             placeholder='E-mail'
             autoCorrect={false}
-            onChangeText={()=>{}}
+            onChangeText={(text) => setEmail(text)}
             />
 
             <TextInput style={StylesLoginPage.Interface}
             placeholder='Senha'
             secureTextEntry
             autoCorrect={false}
-            onChangeText={()=>{}}
+            onChangeText={(text) => setPassword(text)}
             />
 
-            <TouchableOpacity style={StylesLoginPage.Botao}>
+            <TouchableOpacity onPress={handleLogin} style={StylesLoginPage.Botao}>
               <Text style={StylesLoginPage.TextoBotao}>Acessar</Text>
             </TouchableOpacity>
 
