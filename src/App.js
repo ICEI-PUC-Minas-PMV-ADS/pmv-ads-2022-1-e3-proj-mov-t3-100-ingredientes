3c4cb50d611ff020b9b24b9808db02ef72c8ad96
João Sena <joasoenadainese@gmail.com>
2022-04-21 20:56:15 -0300

    tentativa de merge 2 :skull:

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
-import RegisterPage from './pages/RegisterPage';
 import PostRecipePage from './pages/PostRecipePage';
+import RegisterPage from './pages/RegisterPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import UserProvider from './contexts/UserContext';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="MainPage" screenOptions={{headerShown: false}}>
-        <Stack.Screen name="MainPage" component={MainPage} />
-        <Stack.Screen name="LoginPage" component={LoginPage} />
-        <Stack.Screen name="PostRecipePage" component={PostRecipePage} />
-        <Stack.Screen name="RegisterPage" component={RegisterPage} />
-      </Stack.Navigator>
-    </NavigationContainer>    
+    <UserProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="MainPage" screenOptions={{headerShown: false}}>
+          <Stack.Screen name="MainPage" component={MainPage} />
+          <Stack.Screen name="LoginPage" component={LoginPage} />
+          <Stack.Screen name="PostRecipePage" component={PostRecipePage} />
+          <Stack.Screen name="RegisterPage" component={RegisterPage} />
+        </Stack.Navigator>
+      </NavigationContainer>   
+    </UserProvider> 
   );
 };
 
