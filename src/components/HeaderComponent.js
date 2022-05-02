30680382927d26e624b61061936a3cf3822a33f8
João Sena <joasoenadainese@gmail.com>
2022-05-02 09:04:31 -0300

    Header exibe "conta" caso logado

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,8 +2,10 @@ import { Button } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
 import { View , StatusBar } from 'react-native';
 import { useNavigation} from '@react-navigation/native';
+import { useUser } from '../contexts/UserContext';
 
 const HeaderComponent = () => {
+  const {userSigned} = useUser();
 
   const navigation = useNavigation();
 
@@ -14,7 +16,8 @@ const HeaderComponent = () => {
         backgroundColor = "#ff9930"
         translucent = {false}
         networkActivityIndicatorVisible = {true}/>
-      <Button mode="text" onPress={() => navigation.navigate('LoginPage')}>Login</Button>
+      { userSigned ? <Button mode="text" onPress={() => navigation.navigate('AccountPage')}>Conta</Button> : null }
+      { !userSigned ? <Button mode="text" onPress={() => navigation.navigate('LoginPage')}>Login</Button> : null }
       <Button mode="text" onPress={() => navigation.navigate('MainPage')}>Pesquisar</Button>
       <Button mode="text" onPress={() => navigation.navigate('PostRecipePage')}>Postar</Button>
     </View>  
