c517421577b6c4a5d46d9a08e89054fc78da9724
João Sena <joasoenadainese@gmail.com>
2022-05-06 12:03:46 -0300

    buscando receitas do banco

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,4 +1,5 @@
 import {Text, View, TouchableOpacity} from 'react-native';
+import {useState, useEffect} from 'react';
 import StylesAccountPage from '../styles/StylesAccountPage';
 import StylesGeneric from '../styles/StylesGeneric';
 import HeaderComponent from '../components/HeaderComponent';
@@ -6,14 +7,54 @@ import BodyComponent from '../components/BodyComponent';
 import { useNavigation } from '@react-navigation/native';
 import GenericButtonComponent from '../components/GenericButtonComponent';
 import RecipeListComponent from '../components/RecipeListComponent';
+import { getOwnRecipesByUserId, getFavoriteRecipesByUserId } from '../services/recipes-service';
+import { useUser } from './../contexts/UserContext';
 
 const AccountPage = () => {
     const navigation = useNavigation();
+
+    const {userId} = useUser();
+
+    const [ownRecipes, setOwnRecipes] = useState([]);
+    const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+    const getOwnRecipes = async () =>{
+        getOwnRecipesByUserId({
+            userId: userId,
+        }).then(async response => {  
+          if(response && response.success){
+            console.log("Get own recipes by user id success");
+            setOwnRecipes(response.data);
+          }else{
+            console.log("Get own recipes by user id failed");
+            console.log(response);
+          }
+        })
+    }
+    
+    const getFavoriteRecipes = async () =>{
+        getFavoriteRecipesByUserId({
+            userId: userId,
+        }).then(async response => {  
+          if(response && response.success){
+            console.log("Get favorite recipes by user id success");
+            setFavoriteRecipes(response.data);
+          }else{
+            console.log("Get favorite recipes by user id failed");
+            console.log(response);
+          }
+        })
+    }
+
+
+
+    useEffect(() => {
+      getOwnRecipes();
+      getFavoriteRecipes();
+    }, []);
     
     const dados=[
         {id:'1',name:'Receita 001', imageUrl:'https://s3-alpha-sig.figma.com/img/952f/99ba/987a5e9dba94b0101051366fa648251c?Expires=1652659200&Signature=DhirJ893M7PN~~XMo7HZFlZ5ZxFQBkNDj3vPoKGoFuiLLitvo75H-uM2NWHXe-Dj1PIoImW5QCxp5LeV3gtubztVS0h~5bigyFp0AY4yLRRdkc6HeQ5fyPnaF8ywDfO69w1B8250GK2c7h2QuYndgZSJZg75qFAsrp5S0hnQ1bZv-vQfkSJA8RWAyD7axpMEpFpnk1gKFA4JfGkJLi9PM9lHu7osJXO3YHDsBMJ-3XsiratL4kRKoQqIPwBM5k1gzoYKVLxeu1keNWvvBsn~5SPXF-rm9pUgB3p8QXN-PyldWhvAYkZ6RjrVoYfs-OPppsPezY7FzNws282Re6y8rA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},        {id:'1',name:'Receita 001', imageUrl:'https://s3-alpha-sig.figma.com/img/952f/99ba/987a5e9dba94b0101051366fa648251c?Expires=1652659200&Signature=DhirJ893M7PN~~XMo7HZFlZ5ZxFQBkNDj3vPoKGoFuiLLitvo75H-uM2NWHXe-Dj1PIoImW5QCxp5LeV3gtubztVS0h~5bigyFp0AY4yLRRdkc6HeQ5fyPnaF8ywDfO69w1B8250GK2c7h2QuYndgZSJZg75qFAsrp5S0hnQ1bZv-vQfkSJA8RWAyD7axpMEpFpnk1gKFA4JfGkJLi9PM9lHu7osJXO3YHDsBMJ-3XsiratL4kRKoQqIPwBM5k1gzoYKVLxeu1keNWvvBsn~5SPXF-rm9pUgB3p8QXN-PyldWhvAYkZ6RjrVoYfs-OPppsPezY7FzNws282Re6y8rA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},        {id:'1',name:'Receita 001', imageUrl:'https://s3-alpha-sig.figma.com/img/952f/99ba/987a5e9dba94b0101051366fa648251c?Expires=1652659200&Signature=DhirJ893M7PN~~XMo7HZFlZ5ZxFQBkNDj3vPoKGoFuiLLitvo75H-uM2NWHXe-Dj1PIoImW5QCxp5LeV3gtubztVS0h~5bigyFp0AY4yLRRdkc6HeQ5fyPnaF8ywDfO69w1B8250GK2c7h2QuYndgZSJZg75qFAsrp5S0hnQ1bZv-vQfkSJA8RWAyD7axpMEpFpnk1gKFA4JfGkJLi9PM9lHu7osJXO3YHDsBMJ-3XsiratL4kRKoQqIPwBM5k1gzoYKVLxeu1keNWvvBsn~5SPXF-rm9pUgB3p8QXN-PyldWhvAYkZ6RjrVoYfs-OPppsPezY7FzNws282Re6y8rA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},        {id:'1',name:'Receita 001', imageUrl:'https://s3-alpha-sig.figma.com/img/952f/99ba/987a5e9dba94b0101051366fa648251c?Expires=1652659200&Signature=DhirJ893M7PN~~XMo7HZFlZ5ZxFQBkNDj3vPoKGoFuiLLitvo75H-uM2NWHXe-Dj1PIoImW5QCxp5LeV3gtubztVS0h~5bigyFp0AY4yLRRdkc6HeQ5fyPnaF8ywDfO69w1B8250GK2c7h2QuYndgZSJZg75qFAsrp5S0hnQ1bZv-vQfkSJA8RWAyD7axpMEpFpnk1gKFA4JfGkJLi9PM9lHu7osJXO3YHDsBMJ-3XsiratL4kRKoQqIPwBM5k1gzoYKVLxeu1keNWvvBsn~5SPXF-rm9pUgB3p8QXN-PyldWhvAYkZ6RjrVoYfs-OPppsPezY7FzNws282Re6y8rA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},        {id:'1',name:'Receita 001', imageUrl:'https://s3-alpha-sig.figma.com/img/952f/99ba/987a5e9dba94b0101051366fa648251c?Expires=1652659200&Signature=DhirJ893M7PN~~XMo7HZFlZ5ZxFQBkNDj3vPoKGoFuiLLitvo75H-uM2NWHXe-Dj1PIoImW5QCxp5LeV3gtubztVS0h~5bigyFp0AY4yLRRdkc6HeQ5fyPnaF8ywDfO69w1B8250GK2c7h2QuYndgZSJZg75qFAsrp5S0hnQ1bZv-vQfkSJA8RWAyD7axpMEpFpnk1gKFA4JfGkJLi9PM9lHu7osJXO3YHDsBMJ-3XsiratL4kRKoQqIPwBM5k1gzoYKVLxeu1keNWvvBsn~5SPXF-rm9pUgB3p8QXN-PyldWhvAYkZ6RjrVoYfs-OPppsPezY7FzNws282Re6y8rA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},        {id:'1',name:'Receita 001', imageUrl:'https://s3-alpha-sig.figma.com/img/952f/99ba/987a5e9dba94b0101051366fa648251c?Expires=1652659200&Signature=DhirJ893M7PN~~XMo7HZFlZ5ZxFQBkNDj3vPoKGoFuiLLitvo75H-uM2NWHXe-Dj1PIoImW5QCxp5LeV3gtubztVS0h~5bigyFp0AY4yLRRdkc6HeQ5fyPnaF8ywDfO69w1B8250GK2c7h2QuYndgZSJZg75qFAsrp5S0hnQ1bZv-vQfkSJA8RWAyD7axpMEpFpnk1gKFA4JfGkJLi9PM9lHu7osJXO3YHDsBMJ-3XsiratL4kRKoQqIPwBM5k1gzoYKVLxeu1keNWvvBsn~5SPXF-rm9pUgB3p8QXN-PyldWhvAYkZ6RjrVoYfs-OPppsPezY7FzNws282Re6y8rA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},        {id:'1',name:'Receita 001', imageUrl:'https://s3-alpha-sig.figma.com/img/952f/99ba/987a5e9dba94b0101051366fa648251c?Expires=1652659200&Signature=DhirJ893M7PN~~XMo7HZFlZ5ZxFQBkNDj3vPoKGoFuiLLitvo75H-uM2NWHXe-Dj1PIoImW5QCxp5LeV3gtubztVS0h~5bigyFp0AY4yLRRdkc6HeQ5fyPnaF8ywDfO69w1B8250GK2c7h2QuYndgZSJZg75qFAsrp5S0hnQ1bZv-vQfkSJA8RWAyD7axpMEpFpnk1gKFA4JfGkJLi9PM9lHu7osJXO3YHDsBMJ-3XsiratL4kRKoQqIPwBM5k1gzoYKVLxeu1keNWvvBsn~5SPXF-rm9pUgB3p8QXN-PyldWhvAYkZ6RjrVoYfs-OPppsPezY7FzNws282Re6y8rA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},
-
-
 
     ]
   return (
@@ -25,7 +66,7 @@ const AccountPage = () => {
         <View style={StylesAccountPage.SectionRecipeList}>
             <Text style={StylesAccountPage.Title}>Minhas Receitas</Text>
             <View style={{flex: 9}}>
-                <RecipeListComponent data={dados}></RecipeListComponent>
+                <RecipeListComponent data={ownRecipes}></RecipeListComponent>
             </View>
             <View style={{flex: 2, marginTop: 15}}>
                 <GenericButtonComponent>Ver todos</GenericButtonComponent>
@@ -35,7 +76,7 @@ const AccountPage = () => {
         <View style={StylesAccountPage.SectionRecipeList}>
             <Text style={StylesAccountPage.Title}>RECEITAS FAVORITAS</Text>
             <View style={{flex: 9}}>
-                <RecipeListComponent data={dados}></RecipeListComponent>
+                <RecipeListComponent data={favoriteRecipes}></RecipeListComponent>
             </View>
             <View style={{flex: 2, marginTop: 15}}>
                 <GenericButtonComponent>Ver todos</GenericButtonComponent>
--- src/services/recipes-service.js
+++ src/services/recipes-service.js
@@ -1,10 +1,59 @@
+import { includes } from 'json-server-auth';
 import API, {BASE_URL} from './webapi-service';
 
 export const getRecipes = async () => {
   try{
     return await API.get(`${BASE_URL}/recipes`).then( 
       response => {
-        console.log(response.data);
+        return { success: true, data: response.data };
+      },
+      error =>{
+        console.log(error);
+        return { success: false, data: response.data };
+      }
+    );
+  }catch(error){
+    console.log("Erro interno. " + error);
+    return null;
+  }
+}
+
+export const getOwnRecipesByUserId = async (props) => {
+  try{
+    return await API.get(`${BASE_URL}/recipes?cratedByUserId=${props.userId}`).then( 
+      response => {
+        return { success: true, data: response.data };
+      },
+      error =>{
+        console.log(error);
+        return { success: false, data: response.data };
+      }
+    );
+  }catch(error){
+    console.log("Erro interno. " + error);
+    return null;
+  }
+}
+
+export const getFavoriteRecipesByUserId = async (props) => {
+  try{
+    return await API.get(`${BASE_URL}/recipes?favoritedByUsersId=${props.userId}`).then( 
+
+      
+      response => {
+        
+
+        let a = response.data;
+
+        console.log(a);
+
+        let x = a.filter(function (z) {
+          return z != undefined;
+        });
+
+        console.log("lista filtrada");
+        console.log(x);
+
         return { success: true, data: response.data };
       },
       error =>{
