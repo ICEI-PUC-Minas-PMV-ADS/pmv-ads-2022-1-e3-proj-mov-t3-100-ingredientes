bf2b95a5155cba9915a433ca92fc79ec9f8529ab
Victor Lopes <vlinvictus@gmail.com>
2022-05-08 22:44:03 -0300

    Esqueleto Main Page
    Esqueleto Main Page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,16 +1,35 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Image } from 'react-native';
 import StylesMainPage from '../styles/StylesMainPage';
 import HeaderComponent from '../components/HeaderComponent';
 import BodyComponent from '../components/BodyComponent';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 const MainPage = () => {
   return (
     <>
     <HeaderComponent></HeaderComponent>
     <BodyComponent>
-      <View>
-      <Text>Teste Página Principal</Text>
+      <View style={StylesMainPage.Pesquisar}>
+      <TextInput placeholder='Pesquisar' 
+      style={StylesMainPage.input}/>
+      <Ionicons name='search' color={'#fff'} size={30} onPress={() => {}} 
+      style={StylesMainPage.search}
+      />
+      </View>
+      <View style={StylesMainPage.ultimo}>
+      <Text style={StylesMainPage.Textouv}>ÚLTIMOS VISTOS</Text>
+      </View>
+      <View style={StylesMainPage.imagemp}>
+        <Image style={StylesMainPage.imagema} onPress={() => {}} source={require('../assets/images/receitaum.png')}/>
+        <Image style={StylesMainPage.imagema} onPress={() => {}} source={require('../assets/images/receita2.png')}/>
+        <Image style={StylesMainPage.imagema} onPress={() => {}} source={require('../assets/images/receita3.png')}/>
 
+      </View>
+      <View style={StylesMainPage.textovi}>
+        <Text style={StylesMainPage.Textode}>Frango Empanado</Text>
+        <Text style={StylesMainPage.Textode}>Porção de Batata</Text>
+        <Text style={StylesMainPage.Textode}>Refeição Mineira</Text>
       </View>
     </BodyComponent>
     </>
--- src/styles/StylesMainPage.js
+++ src/styles/StylesMainPage.js
@@ -1,15 +1,69 @@
 import { StyleSheet } from 'react-native';
 
 const StylesMainPage = StyleSheet.create({
- Tela: {
-    flex:1,
-    alignItems:'center',
-    backgroundColor: '#555',
- },
- Body: {
-   backgroundColor: 'orange',
-   flex: 9
-}
+   Pesquisar: {
+      flexDirection:"row",
+      justifyContent: "space-between",
+      width: '60%',
+      alignSelf: "center",
+      paddingTop: 10,
+      
+   },
+   input: {
+      flex: 1,
+      backgroundColor:"#d3d3d3",
+      borderRadius:10,
+      color:"white",
+      fontSize: 18,
+      paddingHorizontal: 15, 
+      shadowOpacity: 5,
+      shadowRadius: 10,
+      
+   },
+   search: {
+      paddingHorizontal: 15, 
+   },
+   Textouv: {
+      fontSize: 20,
+      color: "#555",
+   },
+   ultimo: {
+      paddingTop: 65,
+      paddingLeft: 20,
+   },
+   imagemp: {
+      flexDirection:"row",
+      justifyContent: "space-between",
+      alignSelf: "center",
+      paddingTop: 60,
+      
+      
+   },
+   imagema: {
+      height: 80,
+      width: 80,
+      padding: 50,
+      margin: 10,
+      
+      
+   },
+   Textode: {
+      fontSize: 10,
+      color: "#555",
+      justifyContent: "space-between",
+      alignSelf: "center",
+      paddingLeft: 20,
+      paddingRight: 20,
+   
+   },
+   textovi: {
+      flexDirection:"row",
+      
+      alignSelf: "center",
+      padding: 30,
+
+   },
+   
 });
 
 export default StylesMainPage;
