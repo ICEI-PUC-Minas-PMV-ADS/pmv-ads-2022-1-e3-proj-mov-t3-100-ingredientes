504e8b6c8882d50c7b9c7ab5d8117175f4ba35a7
Gabriel Ilidio <gilidio8@gmail.com>
2022-05-12 20:57:47 -0300

    Alteração na PostRecipePage

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -15,11 +15,7 @@ const MyAccount = () => {
         {id:'3',Nome:'Receita 003',Imagem:semImagem},
         {id:'4',Nome:'Receita 004',Imagem:semImagem},
         {id:'5',Nome:'Receita 005',Imagem:semImagem},
-        {id:'6',Nome:'Receita 006',Imagem:semImagem},
-   //     {id:'7',Nome:'Receita 007',Imagem:semImagem},
-   //     {id:'8',Nome:'Receita 008',Imagem:semImagem},
-   //     {id:'9',Nome:'Receita 009',Imagem:semImagem},
-   //     {id:'10',Nome:'Receita 010',Imagem:semImagem},
+        {id:'6',Nome:'Receita 006',Imagem:semImagem}
     ]
   return (
    <>
