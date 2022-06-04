38a0149d9d4cb0b8d7d2b982067c0fa5dac7b021
João Sena <joasoenadainese@gmail.com>
2022-06-04 20:01:08 -0300

    ajuste estilo post recipe

--- src/pages/PostRecipePage.js
+++ src/pages/PostRecipePage.js
@@ -1,15 +1,13 @@
-import { View, Text, TextInput,TouchableOpacity,Image, StyleSheet,KeyboardAvoidingView} from 'react-native';
+import { View, Text, TextInput,TouchableOpacity,Image, KeyboardAvoidingView} from 'react-native';
 import HeaderComponent from './../components/HeaderComponent';
 import BodyComponent from '../components/BodyComponent';
 import { PostRecipes } from '../services/recipes-service';
 import { useState } from 'react';
-import StylesLoginPage from '../styles/StylesLoginPage';
+import StylesPostRecipePage from '../styles/StylesPostRecipePage';
+import StylesGeneric from '../styles/StylesGeneric';
 import { useUser } from './../contexts/UserContext';
-import {redirectUnauthenticatedToLogin} from '../services/auth-service'
 
 const PostRecipePage = () => {
-  //redirectUnauthenticatedToLogin();
-
   const {userId} = useUser();
 
   const [imgUrl, setImgUrl] = useState('');
@@ -39,15 +37,9 @@ const PostRecipePage = () => {
         favoritedByUserIdList: []
 
       }).then( response => {
-
         if(response && response.success){
-          console.log('Recipes success');
-          console.log(response);
-          console.warn('Receita registrada com Sucesso');
           setTrueFeedBack(true);
-
         }else{
-          console.log('Recipes failed');
           console.log(response);
           console.warn('Erro ao criar nova receita');
         }
@@ -60,146 +52,63 @@ const PostRecipePage = () => {
   return (
   <>
     <HeaderComponent></HeaderComponent>
-  <BodyComponent>
-  <KeyboardAvoidingView style={estilo.Screen} behavior="padding">
-      <Text style={estilo.texto}>PUBLIQUE SUA RECEITA</Text>
-      <View style={{flex:30,alignItems:'center' }}>
-        <Text style={estilo.texto}>PHOTO</Text>
-        {imgUrl != '' && <Image style={estilo.logo} source={{uri:imgUrl}}/>}
-        {imgUrl == '' && <Image style={estilo.logo} source={require('../assets/images/Ramen.png')}/>}
-      </View>
-      <View style={{flex:70}}>
-        <TextInput
-        style={estilo.url}
-        placeholder="imagem por url"
-        autoCorrect={true}
-        onChangeText={(text) => setImgUrl(text)}
-        />
-        <View>
-          <Text style={estilo.texto}>TITULO DA RECEITA</Text>
-          <TextInput
-            style={estilo.inputArea}
-            placeholder ="Titulo da Receita..." 
-            autoCorrect={true}
-            onChangeText={(text) => setTitle(text)}
-          />
-        </View>  
-        <View>
-          <Text style={estilo.texto}>INGREDIENTES</Text>  
-          <TextInput
-            style={estilo.inputArea}
-            placeholder="banana, farinha de tringo, ovos ..."
-            autoCorrect={true}
-            onChangeText={(text) => setIngredients(text)}
-          />
+    <BodyComponent>
+    <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+        <View style={{flex: 3, alignItems:'center' }}>
+          <Text style={StylesGeneric.GenericTitle}>PUBLIQUE SUA RECEITA</Text>
+          {imgUrl != '' && <Image style={StylesPostRecipePage.logo} source={{uri:imgUrl}}/>}
+          {imgUrl == '' && <Image style={StylesPostRecipePage.logo} source={require('../assets/images/Ramen.png')}/>}
         </View>
-        <View>  
-          <Text style={estilo.texto}>MODO DE PREPARO</Text>
+        <View style={{flex: 7}}>
           <TextInput
-            style={estilo.inputArea}
-            placeholder="Misture tudo e coloque no forno..."
-            autoCorrect={true}
-            onChangeText={(text) => setInstrucions(text)}
+          style={StylesPostRecipePage.inputImgUrl}
+          placeholder="URL da imagem"
+          autoCorrect={true}
+          onChangeText={(text) => setImgUrl(text)}
           />
+        
+          <View>
+            <Text style={StylesPostRecipePage.Label}>Título da Receita</Text>
+            <TextInput
+              style={StylesPostRecipePage.inputArea}
+              placeholder ="Bolo de morango" 
+              autoCorrect={true}
+              onChangeText={(text) => setTitle(text)}
+            />
+          </View>  
+          <View>
+            <Text style={StylesPostRecipePage.Label}>Ingredientes</Text>  
+            <TextInput
+              style={StylesPostRecipePage.inputArea}
+              placeholder="Banana, farinha de tringo, ovos ..."
+              autoCorrect={true}
+              onChangeText={(text) => setIngredients(text)}
+            />
+          </View>
+          <View>  
+            <Text style={StylesPostRecipePage.Label}>Modo de Preparo</Text>
+            <TextInput
+              style={StylesPostRecipePage.inputArea}
+              placeholder="Misture tudo e coloque no forno..."
+              autoCorrect={true}
+              onChangeText={(text) => setInstrucions(text)}
+            />
+          </View>
+          <View>
+            <Text style={StylesGeneric.LabelGenericBlack}>{ trueFeedBack ? 'A Receita foi enviada com sucesso!!!' : null }</Text>
+            <Text style={StylesGeneric.LabelGenericBlack}>{ falseFeedBack ? 'Possui algum local em branco, favor verificar' : null }</Text>
+          </View>
+        </View> 
+        <View style={{flex: 1}}>
+          <TouchableOpacity style={StylesPostRecipePage.PostRecipeLink} onPress={()=> handlePostRecipes()} > 
+            <Text style={StylesGeneric.LinkGeneric}>Publicar Receita</Text>
+          </TouchableOpacity>
         </View>
-        <View>
-          <Text style={estilo.texto}>{ trueFeedBack ? 'A Receita foi enviada com sucesso!!!' : null }</Text>
-          <Text style={estilo.texto}>{ falseFeedBack ? 'Possui algum local em branco, favor verificar' : null }</Text>
-        </View>
-        <TouchableOpacity style={estilo.CreateAccount} onPress={()=> handlePostRecipes()} > 
-          <Text style={estilo.LinkGeneric}>Publicar Receita</Text>
-        </TouchableOpacity>
-      </View> 
     </KeyboardAvoidingView>
 
-  </BodyComponent>
+    </BodyComponent>
   </>
   );
 }
-const estilo = StyleSheet.create({
-  logo:{
-      flex:1, 
-      width:175, 
-      height:150,
-      borderRadius: 7,
-      overflow: 'hidden',
-      borderWidth: 1,
-      resizeMode: 'cover',
-      shadowOffset: {
-        width: 0,
-        height: 3,
-      },
-       shadowOpacity: 0.40,
-      shadowRadius: 4.84,
-      shadowColor: "#000",
-      marginTop:2,
 
-      
-  },
-  ImageSection: {
-    flex:1,
-    backgroundColor:'#000',
- },
- inputArea: {
-  width: '90%',
-  height: 70,
-  alignItems: 'center',
-  alignSelf: 'center',
-  borderRadius:7,
-  shadowOffset: {
-    width: 0,
-    height: 3,
-  },
-   shadowOpacity: 0.40,
-  shadowRadius: 3.84,
-  shadowColor: "#000",
-  elevation: 5,
-  backgroundColor: '#fff',
-
-},
-button:{
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 20,
-    width: '50%',
-    borderRadius:7,
-    alignSelf: 'center',
- },
-texto:{
-fontFamily:'',
-margin: 15,
-textAlign: 'center',
-},
-Screen: {
-  flex: 1,
-  backgroundColor: '#fff7',
-},
-url: {
-  width: '90%',
-  height: 40,
-  alignItems: 'center',
-  alignSelf: 'center',
-  borderRadius:7,
-  shadowOffset: {
-    width: 0,
-    height: 3,
-  },
-   shadowOpacity: 0.40,
-  shadowRadius: 3.84,
-  shadowColor: "#000",
-  elevation: 5,
-  backgroundColor: '#fff',
-  marginTop: 10
-},
-LinkGeneric: {
-  fontSize: 20,
-  color: '#E05D25',
-  fontWeight: '700',
-  marginTop:20
-},
-CreateAccount:{
-  flexDirection: 'row',
-  alignSelf: 'center'
-}
-})
 export default PostRecipePage;
